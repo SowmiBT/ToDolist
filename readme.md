a4762528fd9db9a928bf4a309027d72169b5c8a0
Sowmiya <4770436@lowes.com>
2025-01-06 07:46:27 +0530

    initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+1. add todo form
+2. todo list
+3. delete todo
+4. clear list
+
+
+store > localstorage
---
+++ scripts/index.js
@@ -0,0 +1,90 @@
+const storeKey = 'TODO_LIST';
+const completedStoreKey = 'COMPLETED_TODO_LIST';
+
+function extractFormData(formdata) {
+    let obj = {};
+    for (const [key, value] of formdata.entries()) {
+        obj[key] = value;
+    }
+    return obj;
+}
+
+function updateLocalStore(value) {
+    const store = localStorage.getItem(storeKey) ? JSON.parse(localStorage.getItem(storeKey)) : [];
+    store.push(value);
+    localStorage.setItem(storeKey, JSON.stringify(store));
+}
+
+function generateTodo(todo, parent = 'pending-todos') {
+    const todos = document.getElementById(parent);
+
+    const todoEle = document.createElement('div');
+    todoEle.className = 'todo';
+
+    const checkboxEle = document.createElement('input');
+    checkboxEle.type = 'checkbox';
+    checkboxEle.name = `complete-${todo.title}`;
+    checkboxEle.id = `complete-${todo.title}`;
+    todoEle.appendChild(checkboxEle);
+
+    const titleEle = document.createElement('span');
+    titleEle.textContent = todo.title;
+    todoEle.appendChild(titleEle);
+
+    checkboxEle.onclick = function () {
+        const key = parent === 'pending-todos' ? storeKey : completedStoreKey;
+        const store = localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key)) : [];
+        const newStore = store.filter((item) => item.title !== todo.title);
+        localStorage.setItem(key, JSON.stringify(newStore));
+
+        const completedTodoStore = localStorage.getItem(completedStoreKey) ? JSON.parse(localStorage.getItem(completedStoreKey)) : [];
+        completedTodoStore.push(todo);
+        localStorage.setItem(completedStoreKey, JSON.stringify(completedTodoStore));
+
+        todoEle.remove();
+        generateTodo(todo, 'completed-todos');
+        updateCompletedCount();
+    };
+
+    todos.insertBefore(todoEle, todos.firstChild);
+}
+
+function addTodo(todo) {
+    generateTodo(todo);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const formdata = new FormData(form);
+    const formValues = extractFormData(formdata);
+    updateLocalStore(formValues);
+    form.reset();
+    addTodo(formValues);
+    return false;
+}
+
+function loadTodos() {
+    const store = localStorage.getItem(storeKey) ? JSON.parse(localStorage.getItem(storeKey)) : [];
+    if (store.length > 0) {
+        store.forEach((todo) => {
+            generateTodo(todo);
+        });
+    }
+
+    const completedTodoStore = localStorage.getItem(completedStoreKey) ? JSON.parse(localStorage.getItem(completedStoreKey)) : [];
+    if (completedTodoStore.length > 0) {
+        completedTodoStore.forEach((todo) => {
+            generateTodo(todo, 'completed-todos');
+        });
+    }
+
+    updateCompletedCount();
+}
+
+function updateCompletedCount() {
+    const completedCount = JSON.parse(localStorage.getItem(completedStoreKey)).length;
+    document.getElementById('completed-count').textContent = completedCount;
+}
+
+loadTodos();
